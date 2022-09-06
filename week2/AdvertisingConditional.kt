59baa5b6a3472553984d1269a73b8eb981da48df
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-06 10:58:26 -0500

    Create AdvertisingConditional.kt

---
+++ week2/AdvertisingConditional.kt
@@ -0,0 +1,12 @@
+class AdvertisingConditional {
+   public static void main(String[] args) {
+      // Write your code here
+      var friendsCount= 500;
+      if (friendsCount > 500) {
+         System.out.println("Adopt a Dog Today!");
+      } else {
+         System.out.println ("Buy Cat Food At 20% Off");
+      }
+
+   }
+}
