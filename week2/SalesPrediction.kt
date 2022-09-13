9bdd8040f6d84d5d04200be96554dc102405c0fa
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-13 08:58:11 -0500

    Create SalesPrediction.kt

---
+++ week2/SalesPrediction.kt
@@ -0,0 +1,12 @@
+public class SalesPrediction
+{
+    public static void main(String[] args)
+    {
+        int x = 4600000;
+        double y = 0.62;
+
+        double total = x * y;
+
+        System.out.println("The East Coast will generate $ " + total + " this year. ");
+    }
+}
