87cc39eeb5e56ddd33a63ba1e999b313a8d1f776
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-13 08:49:09 -0500

    Create AnnualIncome.kt

---
+++ week2/AnnualIncome.kt
@@ -0,0 +1,11 @@
+public class AnnualIncome{
+
+    public static void main(String[] args) {
+        String name = "Joe Mahoney";
+        int age=26;
+        double annualpay=100000.0;
+
+        System.out.println("My name is "+name+" my age is "+age+" and I hope to earn $"+annualpay+" per year.");
+    }
+
+}
