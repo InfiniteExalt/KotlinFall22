6557adcdc669387689ed781a23b19c1fdf3904c4
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-20 11:05:21 -0500

    Create RomanNumerals.kt

---
+++ Week3/RomanNumerals.kt
@@ -0,0 +1,37 @@
+fun main(){
+        print("Enter a number between 1-10 >> ")
+        val number = Integer.valueOf(readLine())
+
+    if (number== 1){
+        print("The roman value is I ")
+    }
+    if (number == 2){
+        print("The roman value is II ")
+    }
+    if (number == 3){
+        print("The roman value is III ")
+    }
+    if (number == 4){
+        print("The roman value is IV ")
+    }
+    if (number == 5){
+        print("The roman value is V ")
+    }
+    if (number == 6){
+        print("The roman value is VI ")
+    }
+    if (number == 7){
+        print("The roman value is VII ")
+    }
+    if (number == 8){
+        print("The roman value is VIII ")
+    }
+    if (number == 9){
+        print("The roman value is IX ")
+    }
+    if (number == 10){
+        print("The roman value is 10 ")
+    }
+    else
+        print("you number is out of range")
+}
