a7cd8e7524b3665d29d38040a487377eebd0745a
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-20 09:53:49 -0500

    Update AnnualIncome.kt

--- week2/AnnualIncome.kt
+++ week2/AnnualIncome.kt
@@ -1,11 +1,9 @@
-public class AnnualIncome{
-
-    public static void main(String[] args) {
-        String name = "Joe Mahoney";
-        int age=26;
-        double annualpay=100000.0;
-
-        System.out.println("My name is "+name+" my age is "+age+" and I hope to earn $"+annualpay+" per year.");
+object AnnualIncome {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val name = "Joe Mahoney"
+        val age = 26
+        val annualpay = 100000.0
+        println("My name is $name my age is $age and I hope to earn $$annualpay per year.")
     }
-
 }
