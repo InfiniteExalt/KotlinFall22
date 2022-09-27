709274cf0852ec853a5af512962d8284bf8e1850
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-27 09:22:13 -0500

    Create RaceTime.kt

---
+++ Week3/RaceTime.kt
@@ -0,0 +1,47 @@
+import java.util.*
+
+object RaceTime {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        //Runner1 info
+        val scan = Scanner(System.`in`)
+        println("Enter name and time:")
+        //String input
+        val runner1 = scan.nextLine()
+        //Numerical Input
+        val time1 = scan.nextInt()
+
+        //Runner2 info
+        val scan2 = Scanner(System.`in`)
+        println("Enter name and time:")
+        //String input
+        val runner2 = scan.nextLine()
+        //Numerical Input
+        val time2 = scan2.nextInt()
+
+        //Runner3 info
+        val scan3 = Scanner(System.`in`)
+        println("Enter name and time:")
+        //String input
+        val runner3 = scan.nextLine()
+        //Numerical Input
+        val time3 = scan3.nextInt()
+
+        // Output input from user
+        println("Runner: $runner1")
+        println("Time: $time1")
+        println("Runner: $runner2")
+        println("Time: $time2")
+        println("Runner: $runner3")
+        println("Time: $time3")
+        if (time1 >= time2) {
+            if (time2 >= time3) println("In order $runner3 $runner2 $runner1") else if (time3 >= time1) println("In order $runner2 $runner1 $runner3") else if (time1 >= time3) println(
+                "In order $runner2 $runner3 $runner1"
+            )
+        } else {
+            if (time3 >= time2) println("In order $runner1 $runner2 $runner3") else if (time3 >= time1) println("In order $runner1 $runner3 $runner2") else if (time1 >= time3) println(
+                "In order $runner3 $runner1 $runner2"
+            )
+        }
+    }
+}
