d971719bd86dd74aedfb0d0cd05474baf87bdd17
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-27 09:35:41 -0500

    Update AdvertisingConditional.kt

--- week2/AdvertisingConditional.kt
+++ week2/AdvertisingConditional.kt
@@ -1,12 +1,9 @@
-internal object AdvertisingConditional {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            // Write your code here
-            val friendsCount = 500
-            if (friendsCount > 500) {
-                println("Adopt a Dog Today!")
-            } else {
-                println("Buy Cat Food At 20% Off")
-            }
+fun main() {
+        // Write your code here
+        val friendsCount = 500
+        if (friendsCount > 500) {
+            println("Adopt a Dog Today!")
+        } else {
+            println("Buy Cat Food At 20% Off")
         }
     }
