2e5bd99298bef0019119ba3c34cd290bc728bb8b
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-27 09:42:14 -0500

    Update StringManipulator.kt

--- week2/StringManipulator.kt
+++ week2/StringManipulator.kt
@@ -1,17 +1,15 @@
-import java.util.Scanner;
-public class StringManipulator{
-public static void main(String[] args){
-        Scanner info=new Scanner(System.in);
-        System.out.println("Enter the name of your favourite city");
-        String city=new String();
-        city=info.next();
-        int length=city.length();
-        String upper=new String();
-        upper=city.toUpperCase();
-        String lower=new String();
-        lower=city.toLowerCase();
-        String first=new String();
-        first=String.valueOf(city.charAt(0));
-        System.out.println("You entered the name of your city: "+city+"\n the number of character in your city are: "+length+"\n Your city name in upper case is: "+upper+"\n Your city name in lower case is: "+lower+"\n the first letter of your city is: "+first);
-        }
-}
+import java.util.*
+    fun main() {
+        val info = Scanner(System.`in`)
+        println("Enter the name of your favourite city:")
+        var city = String()
+        city = info.next()
+        val length = city.length
+        var upper = String()
+        upper = city.uppercase(Locale.getDefault())
+        var lower = String()
+        lower = city.lowercase(Locale.getDefault())
+        var first = String()
+        first = city[0].toString()
+        println("You entered the name of your city: $city\n the number of character in your city are: $length\n Your city name in upper case is: $upper\n Your city name in lower case is: $lower\n the first letter of your city is: $first")
+    }
