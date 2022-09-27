74655dea49f89bb0b87f39aa46fe0ac43ab1423c
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-27 10:20:28 -0500

    Update BMI.kt

--- Week3/BMI.kt
+++ Week3/BMI.kt
@@ -1,8 +1,5 @@
-import java.util.*
-
-object Exercise6 {
-    @JvmStatic
-    fun main(Strings: Array<String>) {
+import java.util.Scanner
+    fun main() {
         val input = Scanner(System.`in`)
         //Here the weight and measurements will be imputed
         print("Input weight in pounds: ")
@@ -10,12 +7,11 @@ object Exercise6 {
         print("Input height in inches: ")
         val inches = input.nextDouble()
         //This is the equation for Body Mass Index
-        val BMI = weight * 0.45359237 / (inches * 0.0254 * inches * 0.0254)
-        print("Body Mass Index is $BMI\n")
+        val bmi = weight * 0.45359237 / (inches * 0.0254 * inches * 0.0254)
+        print("Body Mass Index is $bmi\n")
         //This will print weather it is over weight or if they are underweight
-        if (BMI > 25){
+        if (bmi > 25){
             println("This person in overweight")
         }else
             println("Person is underweight")
     }
-}
