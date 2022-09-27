fda3721f78b410a611da3baaf2cf2f815c880b76
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-27 08:48:07 -0500

    Update MassAndWeight.kt

--- Week3/MassAndWeight.kt
+++ Week3/MassAndWeight.kt
@@ -1,4 +1,5 @@
 fun main(){
+    //User will imput the mass in newtons
     println("Enter mass in newtons: ")
     val mass = Integer.valueOf(readLine())
     if (mass>1000) {
@@ -7,6 +8,8 @@ fun main(){
             if (mass<10)
                 println("mass is too light")
         }
+    //the weight equation for mass
 var weight=(mass * 9.8)
 println("The weight is: $weight")
+//Will output the conversion
 }
