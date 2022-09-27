205787fef545dd712f02b60375cdf902e6198282
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-27 10:36:41 -0500

    Update bankCharges.kt

--- Week3/bankCharges.kt
+++ Week3/bankCharges.kt
@@ -1,45 +1,26 @@
-import java.util.Scanner; 
-import java.text.DecimalFormat;
-
-public class BankCharges {
-
-	public static void main(String[] args) {
-		double BanksFee 	 = 10.00;
-		double LessThan20	 = 0.10 ;
-		double Between20_39  = 0.08 ; 
-		double Between40_59	 = 0.06 ;
-		double MoreThan60 	 = 0.04 ;
-		double TotalFee		 = 0	;
-		int    Checks		 = 0 	;
-		
-		Scanner userInput = new Scanner(System.in);
-		
-		System.out.println("Please enter the number of checks written this month:");
-		Checks = userInput.nextInt();
-		
-		if ( Checks < 20){
-			TotalFee = (Checks * LessThan20) + BanksFee;
-			System.out.print("This month's service fees are $ "+TotalFee);
-		}
-		
-		else if (Checks == 20 || Checks < 40) {
-			TotalFee = (Checks * Between20_39) + BanksFee;
-			System.out.print("This month's service fees are $ "+TotalFee);
-			
-		}
-		
-		else if (Checks == 40 || Checks < 60) {
-			TotalFee = (Checks * Between40_59) + BanksFee;
-			System.out.print("This month's service fees are $ "+TotalFee);
-			
-		}
-		
-		else if (Checks > 60) {
-			TotalFee = (Checks * MoreThan60) + BanksFee;
-			System.out.print("This month's service fees are $ "+TotalFee);
-			
-		}
-		
-	}
-
-}
+import java.util.*
+    fun main() {
+        val banksFee = 10.00
+        val lessThan20 = 0.10
+        val between2039 = 0.08
+        val between4059 = 0.06
+        val moreThan60 = 0.04
+        var totalCharge = 0.0
+        var checks = 0
+        val userInput = Scanner(System.`in`)
+        println("Please enter the number of checks written this month:")
+        checks = userInput.nextInt()
+        if (checks < 20) {
+            totalCharge = checks * lessThan20 + banksFee
+            print("This month's service fees are $ $totalCharge")
+        } else if (checks == 20 || checks < 40) {
+            totalCharge = checks * between2039 + banksFee
+            print("This month's service fees are $ $totalCharge")
+        } else if (checks == 40 || checks < 60) {
+            totalCharge = checks * between4059 + banksFee
+            print("This month's service fees are $ $totalCharge")
+        } else if (checks > 60) {
+            totalCharge = checks * moreThan60 + banksFee
+            print("This month's service fees are $ $totalCharge")
+        }
+    }
