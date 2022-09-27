60bd24f644c3460df272c54f824ba4fd96b966fd
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-27 08:52:55 -0500

    Update SortedNames.kt

--- Week3/SortedNames.kt
+++ Week3/SortedNames.kt
@@ -7,6 +7,7 @@ object Sort3Strings {
         var str1: String
         var str2: String
         var str3: String
+        //Enter the names
         print("Enter first Name: ")
         str1 = scanner.next()
         print("Enter second Name: ")
@@ -16,6 +17,7 @@ object Sort3Strings {
         str1 = str1.lowercase(Locale.getDefault())
         str2 = str2.lowercase(Locale.getDefault())
         str3 = str3.lowercase(Locale.getDefault())
+        //Here it will compare the names based on the letters/string
         if (str1.compareTo(str2) < 0 && str1.compareTo(str3) < 0) {
             println(str1)
             if (str2.compareTo(str3) < 0) {
@@ -27,6 +29,7 @@ object Sort3Strings {
             }
         } else if (str2.compareTo(str1) < 0 && str2.compareTo(str3) < 0) {
             println(str2)
+            //Here it will compare and print in alphabetical order
             if (str1.compareTo(str3) < 0) {
                 println(str1)
                 println(str3)
