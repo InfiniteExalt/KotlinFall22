395fb5a746595edba3e9fbf0dda192836fa2f3c7
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-27 08:42:47 -0500

    Update MagicDate.kt

--- Week3/MagicDate.kt
+++ Week3/MagicDate.kt
@@ -1,5 +1,4 @@
 import java.util.*
-
 object MagicDate {
     @JvmStatic
     fun main(args: Array<String>) {
@@ -7,6 +6,7 @@ object MagicDate {
         val day: Int
         val month: Int
         val two_digit_year: Int
+        // here user will input a date, month, and a two digit year
         print("Enter the value of day: ")
         day = `in`.nextInt()
         `in`.nextLine()
@@ -17,5 +17,6 @@ object MagicDate {
         two_digit_year = `in`.nextInt()
         `in`.nextLine()
         if (day * month == two_digit_year) println("This date is magic!") else println("This date is not magic.")
+        //Here the equation will be executed if true it will be magic and if not it will say it is not magic
     }
 }
