2b8664608458ca157ea1f180bef1f1e209a84aa8
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2022-09-29 13:19:23 -0500

    Create Game.kt

---
+++ Week4/Game.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var correct = 0
+    val colors = arrayOf("Red", "Green", "Blue", "Orange", "Yellow")
+    for(i in 1..10){
+        val color = colors.random()
+        println("Try to guess the random color out of: Red, Green, Blue, Orange, Yellow")
+        val guess = readLine()
+        if(guess==color){
+            println("That's right!")
+            correct++
+        }
+        println("The correct color is $color")
+    }
+    println("Correct guesses: $correct/10")
